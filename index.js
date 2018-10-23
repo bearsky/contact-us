58c4c3853f69eb73747734c92ab3d13d2d4b243f
bearsky <saniabearsky@gmail.com>
2018-10-23 14:55:58 +0300

    Procfile

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const formController = require('./controllers/formController'); // TODO: require all controllers via index
 
 const app = express();
+const config = require('./config');
 
 // set view engine
 app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }));
@@ -22,5 +23,5 @@ app.get('/', function (req, res) { // TODO: add homepage
     res.render('index');
 });
 
-app.listen(3000);
-console.log("App is running at http://localhost:3000");
+app.listen(config.PORT);
+console.log("App is running at : " + config.PORT);
